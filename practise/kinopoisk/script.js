fb1190d0eb3562bb73f7d482385ab61f17396821
ThunDer <thunder00104@gmail.com>
2025-06-23 18:31:57 +0300

    Fixed kinopoisk

--- practise/kinopoisk/script.js
+++ practise/kinopoisk/script.js
@@ -1,38 +1,59 @@
-const videosWrapper = document.querySelector('[data-video="wrapper"]')
+class RenderVideos {
+  #API_URL =
+    "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_MOVIES&page=1"
+  #API_KEY = "2bc5f7de-a6c1-4314-844b-1ed661b25d47"
 
-const renderVideos = function (array) {  
-  array.forEach(element => {
-     videosWrapper.insertAdjacentHTML('beforeend', `
+  constructor(video) {
+    this.videosWrapper = video
+  }
+
+  createVideo(video) {
+    return `
    <div class="video">
         <img
-          src="${element.posterUrlPreview}"
-          alt="ivanzolo"
+          src="${video.posterUrlPreview}"
+          alt=""
           class="video__img"
         />
-        <div class="video__rating">${element.ratingKinopoisk}</div>
-        <h5 class="video__title">${element.nameRu}</h5>
-        <p class="video__genre">${element.genres[0].genre}</p>
+        <div class="video__rating">${video.ratingKinopoisk}</div>
+        <h5 class="video__title">${
+          video.nameRu || "Ошибка: название не найдено"
+        }</h5>
+        <p class="video__genre">${
+          video.genres?.[0]?.genre || "Ошибка: жанр не найден"
+        }</p>
       </div>
-      `)
-  });
-  
- 
+      `
+  }
+
+  renderVideos(array) {
+    this.videosWrapper.innerHTML = ""
+    array.forEach((video) => {
+      this.videosWrapper.insertAdjacentHTML('beforeend', this.createVideo(video)) 
+    })
+  }
+
+  async loadData() {
+    try {
+      const response = await fetch(`${this.#API_URL}`, {
+        headers: {
+          "X-API-KEY": `${this.#API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      })
+      if (!response.ok) {
+        console.error(
+          "Опаньки! Что-то не так на стороне сервера. Вернитесь позже"
+        )
+      }
+
+      const data = await response.json()
+      this.renderVideos(data.items)
+    } catch (error) {
+      throw new Error("Ошибка загрузки!")
+    }
+  }
 }
 
-fetch(
-  "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_MOVIES&page=1",
-  {
-    headers: {
-      "X-API-KEY": "898a4dcc-f092-4bad-bf3e-9f9d3d36de62",
-      "Content-Type": "application/json",
-    },
-  }
-)
-  .then((filmData) => {
-    return filmData.json()
-  })
-  .then((info) => {
-    console.log(info)
-    renderVideos(info.items)
-  })
-
+const test = new RenderVideos(document.querySelector('[data-video="wrapper"]'))
+test.loadData()
